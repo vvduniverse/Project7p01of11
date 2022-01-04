ed15ae6c74622106101f01700f763289265337c2
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-04 07:34:46 +0200

    Initial commit

--- Project7p01of11/ContentView.swift
+++ Project7p01of11/ContentView.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    
+    @State private var user = User()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+        .padding()
     }
 }
 
